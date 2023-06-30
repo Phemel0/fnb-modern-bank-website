d3555b85bed71dcdd665f42cf3d3972148efe66c
Ph3me <p.ntsimane@outlook.com>
2023-06-30 19:44:43 +0200

    Added new assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
-import styles from "./styles<";/>
+import styles from "./styles";
 import {
-  Navbar, 
-  Hero, 
-  Stats, 
-  Business, 
-  Billing, 
-  CardDeal, 
-  Testimonials, 
-  Clients, 
-  CTA, 
-  Footer } from './components';
+  Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components';
 
 const App = () => {
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          <Navbar/>
+          <Navbar />
         </div>
       </div>
 
@@ -27,14 +18,14 @@ const App = () => {
 
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}> 
-        <Stats/> 
-        <Business/> 
-        <Billing/> 
-        <CardDeal/> 
-        <Testimonials/> 
-        <Clients/> 
-        <CTA/>
-        <Footer/> 
+        <Stats /> 
+        <Business /> 
+        <Billing /> 
+        <CardDeal /> 
+        <Testimonials /> 
+        <Clients /> 
+        <CTA />
+        <Footer /> 
       </div>
     </div>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,16 @@ const Navbar = () => {
   return (
     <nav className='w-full flex py-6 justify-between navbar'>
         <img src={logo} alt="hoobank" className='w-[124px] h-[32px]'/>
-        <ul className="list-none sm:flex hidden justify-end items-center">
-
+        
+        <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+            {navLinks.map((nav, index) =>(
+              <li key={nav.id} className='font-poppins'>
+                <a href={'#${nav.id}'}>
+                  {nav.title}
+                </a>
+                
+              </li>
+            ))}
         </ul>
     </nav>
   )
