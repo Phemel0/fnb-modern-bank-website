e44e1089781a5e0c635c0e3fc9b5aacd4ae8f4d4
Ph3me <p.ntsimane@outlook.com>
2023-07-05 01:11:07 +0200

    List items in the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,11 @@ const Navbar = () => {
           />
 
           <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
-
+            <ul>
+              <li>
+                
+              </li>
+            </ul>
           </div>
       </div>
       
