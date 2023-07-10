4c7adcbf5d53abb14fe22fbf11c6cc53e038e131
Ph3me <p.ntsimane@outlook.com>
2023-07-10 19:46:00 +0200

    get started buttom complete

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -8,12 +8,16 @@ const GetStarted = () =>  (
       w-[100%] h-[100%] rounded-full`}>
           <div className={`${styles.flexStart} flex-row`}>
             <p className="font-poppins font-medium 
-            text-[18px] leading-[23px]">
+            text-[18px] leading-[23px] mr-2">
               <span className="text-gradient">Get</span>
-              <img src={arrowUp} alt="Arrow" className="w-[23px] 
-              h-[23px] object-contain"/>
             </p>
+            <img src={arrowUp} alt="Arrow" className="w-[23px] 
+              h-[23px] object-contain"/>
           </div>
+          <p className="font-poppins font-medium 
+            text-[18px] leading-[23px]">
+              <span className="text-gradient">Started</span>
+          </p>
       </div>
     </div>
   
