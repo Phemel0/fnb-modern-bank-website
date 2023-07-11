ec362862754176cf9ad82e569c03fd49e7641db8
Ph3me <p.ntsimane@outlook.com>
2023-07-11 22:55:46 +0200

    Created the stats component

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { stats } from "../constants";
+import styles from "../style";
 
-const Stats = () => {
-  return (
-    <div>Stats</div>
-  )
-}
+const Stats = () => (
+  <section className={`${styles.flexCenter}
+  flex-row flex-wrap sm:mb-20 mb-6`}>
+    {stats.map((stats) => (
+      <div key={stats.id} className=''>
+        <h4 className='font-poppins'>{stats.value}</h4>
+        <p>{stats.title}</p>
+      </div>
+    ))}
+  </section>
+)
 
 export default Stats
