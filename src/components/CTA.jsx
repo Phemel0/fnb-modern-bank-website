846eb927cad7d43eb2bfaf4e3696585ff7cba87b
Ph3me <p.ntsimane@outlook.com>
2023-07-19 14:16:32 +0200

    Started with the CTA section

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import styles from "../style";
+import Button from './Button';
 
-const CTA = () => {
-  return (
-    <div>CTA</div>
+const CTA = () => (
+  <section className={`${styles.flexCenter} ${styles.padding}
+  sm:flex-row flex-col bg-black-gradient`}>
+    <div>
+      <h2>Let’s try our service now!</h2>
+      <p>Everything you need to accept card 
+        payments and grow your business anywhere
+        on the planet.
+      </p>
+    </div>
+
+    <div>
+      <Button />
+    </div>
+  </section>
   )
-}
+
 
 export default CTA
